a047a578c475ae972901134f126d39e69f80fa81
Alejandro Rafael Garcia Sanchez <alexgsan14@gmail.com>
2026-06-15 19:38:19 -0600

    Corrige nombre del modelo multimedia

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
-const Multimedia = require('./models/Multimedia');
+const Multimedia = require('./models/multimedia');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
